9a2acea33295debbbceaeaee8c39321bc436a0ab
Khushnood Ali Khan <khushnoodali287@gmail.com>
2024-06-03 17:59:52 +0500

    2nd Topic covered

---
+++ Variables_2nd.py
@@ -0,0 +1,27 @@
+# Variables
+name="Khushnood Ali Khan,"      #assigning values to variables
+age=20
+print("Name",name,"Age",age)    #output ----> Name Khushnood Ali Khan, Age 20
+
+#Check the Data Type of Variable
+print(type(name))       #output ----> str --> string
+print(type(age))        #output ----> int 
+#------------------------------------------------------------------------------------------
+
+# Add Two numbers
+print("10 + 10 =",10+10)    #output -----> 10 + 10 = 20
+
+# OR
+a=10
+print("a + a =",a+a)    #output -----> a + a = 20
+
+#-------------------------------------------------------------------------------------------
+
+#swapping
+b=10
+c=5
+print("B =",b,"C =",c)      #output -----> B = 10 C = 5
+temp=b
+b=c
+c=temp
+print("B =",b,"C =",c)      #now output -----> B = 5 C = 10
