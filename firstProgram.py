aea6058780c9eb671d925b5bd12f1edd29d9acd0
Khushnood Ali Khan <khushnoodali287@gmail.com>
2024-06-03 17:49:00 +0500

    First Program of Python

---
+++ firstProgram.py
@@ -0,0 +1,11 @@
+#Our First program in python
+
+print("Hello World!")
+
+"""
+#print() --> is a function meaning it perform some meaning full task, 
+in python print() function is use to show output on the screen and
+Double quotes " " are used when you want to show some constant string to 
+the user.
+"""
+# """....""" --->> these are comments which are non executable code in a program and used for user readability
