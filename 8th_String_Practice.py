4222c7a51370f46110916d95c66d6abb4db85dd7
Khushnood Ali Khan <khushnoodali287@gmail.com>
2024-06-07 00:43:33 +0500

    Practice Questions on String Topic

---
+++ 8th_String_Practice.py
@@ -0,0 +1,11 @@
+#Practice Questions On String:
+
+#1).Write a program to input user first name and print its length.
+first_name = input("Enter your First Name: ")
+print("Length of your name:",len(first_name))
+#------------------------------------------------------------------------
+
+#2).Write a program to find the Occurence of "a" in a string.
+string = input("\nEnter a String: ")
+print("The number of 'a' in the string is:",string.count("a"))
+#------------------------------------------------------------------------
