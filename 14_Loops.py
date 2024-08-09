ae3dafdf26d9e81d745834bf42864a7d65a33884
Khushnood Ali Khan <khushnoodali287@gmail.com>
2024-08-10 00:57:18 +0500

    Loops

---
+++ 14_Loops.py
@@ -0,0 +1,17 @@
+#Loops :-> Loops are used to repeat instructions.
+#Two Types :-> While Loop and For Loop.
+""" 
+1). While Loop :-> It execute the block of code as long as the given condition become true.
+    Syntex :->
+            while condition :
+                    # block of code
+Example:
+
+while True:
+    print("Hello World!")           #Output --> Hello World!    (infinite time)
+"""
+
+i = 1
+while i <= 5 :
+    print("Hello World!")              #Output --> Hello World!     (print 5 times)
+    i += 1 
